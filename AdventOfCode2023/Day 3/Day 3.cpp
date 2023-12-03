787b89598e039b32cbfb368f2fdd3a4f36ce85dc
DemonwarriorJL <jl.vanwyk@sky.com>
2023-12-03 13:49:41 +0000

    Day 3 Partial

--- AdventOfCode2023/Day 3/Day 3.cpp
+++ AdventOfCode2023/Day 3/Day 3.cpp
@@ -2,10 +2,73 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
+
+std::fstream& GotoLine(std::fstream& file, unsigned int num)
+{
+    file.seekg(std::ios::beg);
+    for (int i = 0; i < num - 1; ++i)
+    {
+        file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    return file;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::fstream newfile;
+    
+    newfile.open("Text3.txt", std::ios::in); //open a file to perform read operation using file object
+    if (newfile.is_open()) {
+
+        std::string lineAbove = "";
+        std::string line = "";
+        std::string lineBelow = "";
+        
+        int numberOfLines = 0;
+
+        //Gets number of lines
+        while (getline(newfile, line))
+        {
+            ++numberOfLines;
+        }
+
+        for (int j = 2; j < numberOfLines; j++) //Loops through lines starting at 3rd row
+        {
+            GotoLine(newfile, j);
+            newfile >> lineBelow;
+
+            GotoLine(newfile, j-1);
+            newfile >> line;
+
+            GotoLine(newfile, j-2);
+            newfile >> lineAbove;
+
+            std::cout << lineAbove << std::endl;
+            std::cout << line << std::endl;
+            std::cout << lineBelow << std::endl;
+            std::cout << "\n" << std::endl;
+        }
+
+        std::cout << "Hello World!\n";
+        //while (getline(newfile, line)) //read data from file object and put it into string.
+        //{
+        //    if (lineBelow == "" || lineBelow == line) 
+        //    {
+
+        //    }
+
+        //    for (int i = 0; i < line.size(); i++) //Move through each line char by char (i is index of current char)
+        //    {
+        //        if (!isdigit(line[i]) || line[i] == '.') //if char is a symbol
+        //        {
+
+        //        }
+        //    }
+        //}
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
