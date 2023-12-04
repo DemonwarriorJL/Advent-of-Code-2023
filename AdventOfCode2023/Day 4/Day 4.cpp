3c8d3a8c6184f5bfad2a51c79e52c5cba51962c2
DemonwarriorJL <91761586+DemonwarriorJL@users.noreply.github.com>
2023-12-04 12:57:36 +0000

    Day 4

--- AdventOfCode2023/Day 4/Day 4.cpp
+++ AdventOfCode2023/Day 4/Day 4.cpp
@@ -2,19 +2,88 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::fstream newfile;
+
+    newfile.open("Text4.txt", std::ios::in); //open a file to perform read operation using file object
+    if (newfile.is_open()) {
+        std::string line;
+        int total = 0;
+
+        while (getline(newfile, line)) //read data from file object and put it into string.
+        {
+            std::vector<int> winningNumbers;
+            std::vector<int> playingNumbers;
+            int lineTotal = 0;
+            std::string m_line = line.substr(line.find(": ") + 2);
+            std::cout << m_line << std::endl;
+
+            bool searchWinningNumbers = true;
+
+            while (m_line != "") 
+            {
+                if (searchWinningNumbers) //searching numbers before |
+                {
+                    if (m_line[0] == '|') //gets to seperator between winning and playing numbers
+                    {
+                        m_line = m_line.substr(m_line.find("| ") + 2);
+                        searchWinningNumbers = false;
+
+                        std::string m_info = m_line.substr(0, 3);
+                        playingNumbers.push_back(stoi(m_info));
+                    }
+                    else //winning numbers
+                    {
+                        std::string m_info = m_line.substr(0, 3);
+                        winningNumbers.push_back(stoi(m_info));
+                    }
+                }
+                else //searching numbers after |
+                {
+                    std::string m_info = m_line.substr(0, 3);
+                    playingNumbers.push_back(stoi(m_info));
+                }
+
+                if ((m_line.size() == 2)) //end of the line
+                {
+                    m_line = "";
+                }
+                else //refresh line for next int
+                {
+                    m_line = m_line.substr(3);
+                }
+            }
+            
+            
+            bool addingBegun = false;
+            for (int number : winningNumbers)
+            {
+                for (int playnumber : playingNumbers) 
+                {
+                    if (number == playnumber)
+                    {
+                        if (addingBegun)
+                        {
+                            lineTotal *= 2;
+                        }
+                        else
+                        {
+                            lineTotal = 1;
+                            addingBegun = true;
+                        }
+                    }
+                }
+            }
+            total += lineTotal;
+        }
+        
+
+        std::cout << total << std::endl;
+        newfile.close(); //close the file object.
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
